391f7053822ffbb38f6a442ac35bdc93147c25d6
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-01 20:09:19 +0000

    js classes have been added

--- script.js
+++ script.js
@@ -13,21 +13,37 @@ class UI{
             {
                 title:'Ali baba ve kirk haramiler',
                 author:'Bilinmiyor',
-                id:'343437'
+                isbn:'343437'
             },
             {
                 title:'Ali baba ve yirmi haramiler',
                 author:'Bilinmiyor',
-                id:'1234567'
+                isbn:'1234567'
             }
         ]
         const books=storedBooks
         books.forEach((book)=>UI.addBookList(book))
     }
+    static addBookList(book){
+        const list=document.querySelector('#book-list')
+        const row=document.createElement('tr')
+        row.innerHTML=`<td>${book.title}</td>
+                       <td>${book.author}</td>
+                       <td>${book.isbn}</td>
+                       <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></td>`;
+        list.appendChild(row)
+
+    }
+
 }
 //store class handle storage
 
 //event: Display Books
+if (document.readyState !== 'loading'){
+    UI.displayBooks()
+}
+else {document.addEventListener('DOMContentLoaded',UI.displayBooks)}
+
 
 //event add a book
 
